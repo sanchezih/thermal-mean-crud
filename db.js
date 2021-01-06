76216d7da17bf077fee516f76b7eb876714ecde3
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2021-01-06 11:33:21 -0300

    agrego rutas

---
+++ db.js
@@ -0,0 +1,28 @@
+// mongodb driver
+const MongoClient = require("mongodb").MongoClient;
+
+const dbConnectionUrl = "mongodb+srv://ignacio:nacho@cluster0.rc9wj.mongodb.net/test?retryWrites=true&w=majority";
+
+function initialize(
+    dbName,
+    dbCollectionName,
+    successCallback,
+    failureCallback
+) {
+    MongoClient.connect(dbConnectionUrl, function (err, dbInstance) {
+        if (err) {
+            console.log(`[MongoDB connection] ERROR: ${err}`);
+            failureCallback(err); // this should be "caught" by the calling function
+        } else {
+            const dbObject = dbInstance.db(dbName);
+            const dbCollection = dbObject.collection(dbCollectionName);
+            console.log("[MongoDB connection] SUCCESS");
+
+            successCallback(dbCollection);
+        }
+    });
+}
+
+module.exports = {
+    initialize
+};
